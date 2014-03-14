a4d0896459493412be385bb96cd868861f7e13c2
darknautic <blue.darknautic@gmail.com>
2014-03-13 20:54:54 -0600

    adding athentication process.

--- client/accounts.js
+++ client/accounts.js
@@ -1,45 +1,61 @@
 Meteor.loginAsAdmin = function(password, callback) {
   //create a login request with admin: true, so our loginHandler can handle this request
-  var loginRequest = {admin: true, password: password};
+    var loginRequest = {admin: true, password: password};
 
-  //send the login request
-  Accounts.callLoginMethod({
-    methodArguments: [loginRequest],
-    userCallback: callback
-  });
-};
+    //send the login request
+    Accounts.callLoginMethod({
+      methodArguments: [loginRequest],
+      userCallback: callback
+    });
+  };
 
 
 
 
 Template.login.events({
+
+  "submit #loginForm": function(e, t){
+    var email = $('#emailBox').val();
+    var passwd= $('#passwdBox').val();
+
+    console.log(check(email, String));    
+    console.log(passwd);
+    
+
+    alert("continue ?");
+
+
+    },
     
   "click button": function () {
-      // template data, if any, is available in 'this'
-      //console.log("You pressed the button222");
-      var email = $("#email").val();
-      var passwd = $("#passwd").val();
-      
-      if (typeof console !== 'undefined')
-        console.log("Login as  " + email);
-        //check entries
-        Meteor.loginWithPassword = function (email, passwd){
-          console.log(Meteor.userId);
-          };
-        alert("Login.....");
+        // template data, if any, is available in 'this'
+        //console.log("You pressed the button222");
+        /*var email = $("#email").val();
+        var passwd = $("#passwd").val();
         
-  },
+        if (typeof console !== 'undefined')
+          console.log("Login as  " + email);
+          //check entries
+          Meteor.loginWithPassword = function (email, passwd){
+            console.log(Meteor.userId);
+            };
+          alert("Login.....");
+          */
+          
+    },
     
   "keydown #message": function(event){
-    if(event.which == 13){
-      // Submit the form
-      //var name = document.getElementById('name');
-      //var message = document.getElementById('message');
-      console.log("You pressed the key");
-  
+      /*if(event.which == 13){
+        // Submit the form
+        //var name = document.getElementById('name');
+        //var message = document.getElementById('message');
+        console.log("You pressed the key");
+    
+      }*/
     }
-  }
-});
+
+  });
+
 
 
 
